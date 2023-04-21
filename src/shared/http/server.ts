a564f62ab7590d79afccbcbfe88c2530384edf45
jvsm123 <jvsm123@outlook.com>
2023-04-20 22:42:12 -0300

    feat: create customers services

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,6 @@
 import 'express-async-errors';
 
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 
 import cors from 'cors';
 
